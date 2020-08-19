cc65a73ac3600c3969923617382eb9f5b7fa030a
JustinBarnard1 <jbarnard0031@gmail.com>
2020-08-19 13:23:48 -0600

    init

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+function draw() {
+    let template = ""
+    actions.forEach(action => {
+        template +=
+
+        //template here
+    })
+    document.getElementById().innerHTML = template
+}
+
+function random() {
+    let index = math.floor(math.random() * (actions.length - 0.01))
+    let currentAction = actions[index]
+    deply(currentAction)
+}
