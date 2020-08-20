0835af8caa575d1caca9c2d036a7e1bbf54cf3c3
JustinBarnard1 <jbarnard0031@gmail.com>
2020-08-19 19:23:08 -0600

    attached message for winner, still nds plyr img

--- app.js
+++ app.js
@@ -26,61 +26,70 @@ function draw() {
             <div class="">
                 <div class="row row-height">
                     <div class="col-12 text-center">
-                        <h2 class="mt-3 text-center">Opponent</h2>
-                        <img class="mt-2" src="./defaultImage.png" alt="">
+                        <h2 class="text-center">Opponent</h2>
+                        <img id="opponent" class="mt-1" src="./defaultImage.png" alt="">
                     </div>
+                    <div class="mx-auto justify-content-center text-center"><h2 id="winner" class="mx-auto justify-content-center text-center"></h2></div>
                 </div>
                 <div class="row">
                     <div class="col-12 text-center">
-                        <img class="" src="./defaultImage.png" alt="">
-                        <h2 class="mt-2 text-center">Player</h2>
-                        <div class="mb-2"><button type="button" name="" id=""
-                                class="btn bg-light text-dark mx-2 btn-width">Rock</button><button type="button" name=""
-                                id="" class="btn bg-light text-dark mx-2 btn-width">Paper</button><button type="button"
-                                name="" id="" class="btn bg-light text-dark mx-2 btn-width">Scissors</button>
+                        <img id="player" class="" src="./defaultImage.png" alt="">
+                        <h2 class="mt-1 text-center">Player</h2>
+                        <div class="mb-1"><button type="button"
+                                class="btn bg-light text-dark mx-2 btn-width" onclick="whoWins('knight')">Knight</button><button type="button"
+                                class="btn bg-light text-dark mx-2 btn-width" onclick="whoWins('wizard')">Wizard</button><button type="button"
+                                class="btn bg-light text-dark mx-2 btn-width" onclick="whoWins('archer')">Archer</button>
                         </div>
                     </div>
                 </div>
             </div>`
 
     document.getElementById('gameBoard').innerHTML = template
-}
 
-function whoWins(action1, action2) {
+}
+// function test() {
+//     console.log("testing")
+// }
+
+function whoWins(action1) {
+    let action2 = random()
+    console.log(action1, action2)
+    document.getElementById("opponent").innerHTML = action2
+    document.getElementById("player").innerHTML = action1
     if (action1 === action2) {
-        return "Tie";
+        return document.getElementById("winner").innerText = "Tie";
     }
     else if (action1 === "knight") {
         if (action2 === "archer") {
-            return "knight wins"
+            return document.getElementById("winner").innerText = "knight wins";
         }
         else if (action2 === "wizard") {
-            return "wizard wins";
+            return document.getElementById("winner").innerText = "wizard wins";
         }
     }
     else if (action1 === "wizard") {
         if (action2 === "archer") {
-            return "archer wins"
+            return document.getElementById("winner").innerText = "archer wins";
         }
         else if (action2 === "knight") {
-            return "wizard wins";
+            return document.getElementById("winner").innerText = "wizard wins";
         }
     }
     else if (action1 === "archer") {
         if (action2 === "wizard") {
-            return "archer win"
+            return document.getElementById("winner").innerText = "archer win";
         }
         else if (action2 === "knight") {
-            return "knight wins";
+            return document.getElementById("winner").innerText = "knight wins";
         }
     }
-
+    draw()
 }
 
 function random() {
-    let index = math.floor(math.random() * (actions.length - 0.01))
-    let currentAction = actions[index]
-    deploy(currentAction)
+    let index = Math.floor(Math.random() * (actions.length - 0.01))
+    let currentAction = actions[index].name
+    return currentAction
 }
 
 function getStarted() {
